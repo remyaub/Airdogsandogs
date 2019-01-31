459aeb0385a415b293261e1c337d716f5e482849
RemyAu <centratobey@gmail.com>
2019-01-31 18:25:46 +0100

    adding all the full project

---
+++ Airdogndog/db/migrate/20190131165133_create_dogs.rb
@@ -0,0 +1,13 @@
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+      t.belongs_to :dogsitter, index: true
+      t.belongs_to :city, index: true
+      t.belongs_to :stroll, index: true
+
+      t.timestamps
+    end
+  end
+end
